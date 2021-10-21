696603d32a2477986686f4706c5ae7859d136392
Gabriel Cordeiro <gabrielpatricyo@gmail.com>
2021-10-21 17:37:01 -0300

    Adicionar ao Carrinho

--- src/screens/login/tela.carrinho.tsx
+++ src/screens/login/tela.carrinho.tsx
@@ -10,30 +10,37 @@ import { AppToolbar } from '../../components/toolbar';
 export interface CarrinhoScreenProps {
 }
 
-export default function CarrinhoScreen (props: CarrinhoScreenProps) {
+export default function CarrinhoScreen(props: CarrinhoScreenProps) {
 
-    const nav = useNavigation();  
+    const nav = useNavigation();
 
     return (
-        <ImageBackground source = { require('./../../../assets/imgs/bgCarrinho1.png')}
-                                      style={styles.background}> 
-        <AppToolbar /> 
-                  <View style={styles.container}>
-                  </View> 
-               
+        <ImageBackground source={require('./../../../assets/imgs/bgCarrinho1.png')}
+            style={styles.background}>
+            <AppToolbar />
+            <View style={styles.container}>
+            </View>
 
-        </ImageBackground>
-      
+            <View style={styles.adicionar}>
+            <TouchableOpacity onPress={() => nav.navigate('home')}>
+                <Button title="Clique para adicionar!"
+                    buttonStyle={{ borderRadius: 15, backgroundColor: '#006BAC', marginTop: 15 }}>
+                </Button>
+            </TouchableOpacity>
+            </View>
+
+        </ImageBackground >
+
     );
 }
 
 const styles = StyleSheet.create({
     background: {
-        width:'100%', 
-        height:'100%',
-        
+        width: '100%',
+        height: '100%',
+
     },
-    container:{
+    container: {
         flex: 1,
         justifyContent: 'center',
         flexDirection: 'row',
@@ -47,5 +54,14 @@ const styles = StyleSheet.create({
         height: 50,
         padding: 2,
 
-    }
+    },
+
+    adicionar: {
+        color: 'white',
+        fontSize: 15,
+        padding: 50,
+        textAlign: 'center',
+        textDecorationLine: 'underline',
+        paddingTop: 20,
+    },
 });
