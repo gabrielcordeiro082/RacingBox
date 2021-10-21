e0e37df34c994298decc6f73d74c3715cff683fd
Gabriel Cordeiro <gabrielpatricyo@gmail.com>
2021-10-21 19:28:46 -0300

    toolbar

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { Header } from 'react-native-elements'
@@ -7,10 +8,11 @@ export interface AppToolbarProps {
 }
 
 export function AppToolbar (props: AppToolbarProps) {
+    const nav = useNavigation()
     return (
       <Header
            containerStyle={{backgroundColor:'white'}}
-           leftComponent={{icon: 'menu', color: 'black', size: 30}}
+           leftComponent={{icon: 'menu', color: 'black', size: 30, onPress:()=>nav.openDrawer()}}
            centerComponent={{text:'Drawer', style:{color: 'black', fontSize: 20}}}
       />
     );
